b5eff6fa829915650216767da1416853ac9b2630
Estarossa <65534927+RileyBerry7@users.noreply.github.com>
2025-04-23 00:14:47 -0700

    Added more non-terminals within the cpp_cfg hash set.

--- compiler/front_end/syntax analysis/cpp_cfg.py
+++ compiler/front_end/syntax analysis/cpp_cfg.py
@@ -12,13 +12,54 @@ terminals = set(TokenType)
 start_state = 'S'
 
 # All possible variables
-non_terminals = set("S",
-                    "BLOCK",
-                    "EXPRESSION",
-                    "OPERATOR",
-                    "OPERAND",
-                    "LINE",
-                    "$")
+nnon_terminals = {
+    # start symbol
+    "TranslationUnit",      # entire source file
+
+    # top-level
+    "DeclarationSeq",       # sequence of declarations/definitions
+    "Declaration",          # e.g. int x;   or   void f() { … }
+
+    # function parts
+    "FunctionDefinition",   # return-type, name, params, body
+    "ParameterList",        # ( type id, type id, … )
+
+    # types
+    "TypeSpecifier",        # int, float, MyClass, …
+
+    # declarators
+    "Declarator",           # pointer, array, identifier
+
+    # statements
+    "StatementSeq",         # { stmt stmt … }
+    "Statement",            # expression-stmt | if-stmt | for-stmt | return-stmt | …
+
+    "ExpressionStatement",  # expr ;
+    "IfStatement",          # if (expr) stmt [ else stmt ]
+    "ForStatement",         # for ( … ) stmt
+    "ReturnStatement",      # return [expr] ;
+
+    # expressions
+    "Expression",           # the generic E
+    "AssignmentExpr",       # E = E
+    "ConditionalExpr",      # E ? E : E
+    "LogicalOrExpr",        # E || E
+    "LogicalAndExpr",       # E && E
+    "EqualityExpr",         # E == E | E != E
+    "RelationalExpr",       # E < E | E > E | …
+    "AdditiveExpr",         # E + E | E - E
+    "MultiplicativeExpr",   # E * E | E / E | E % E
+    "UnaryExpr",            # +E | -E | !E | *E | &E
+    "PrimaryExpr",          # identifier | literal | ( Expression )
+
+    # literals & identifiers (could be tokens instead)
+    "Identifier",           # variable or function names
+    "Literal",              # integer, floating, string, char, bool
+
+    # auxiliary
+    "ArgumentList",         # in a function call: ( E, E, … )
+    "PostfixExpr",          # E [ E ] | E ( ArgumentList? ) | E . id | E -> id
+}
 
 # Rules to yield all terminals
 production_rules = {}
